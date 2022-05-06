3c813b678e908a9cdac9fa463a02a655e4066495
Kevin Finger <kfinger85@gmail.com>
2022-05-06 10:09:44 -0600

    template changes

--- fuel/app/views/m1/usercolor.php
+++ fuel/app/views/m1/usercolor.php
@@ -1,4 +1,3 @@
-    <link type="text/css" media="print" rel="stylesheet" href="https://www.cs.colostate.edu:4444/~kfinger/m1/assets/css/print.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/jquery-ui.js"></script>
     <link href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.16/themes/ui-lightness/jquery-ui.css" rel="stylesheet" type="text/css" />
